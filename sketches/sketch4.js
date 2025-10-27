1a8144d377b7a631153f6bd1434e15945dac32d9
jcabusao <jcabusao@uw.edu>
2025-10-27 12:09:24 -0700

    added step and mile counter

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -6,6 +6,8 @@ registerSketch('sk4', function (p) {
   let feet = [];
   let lastStepTime = 0;
   let startTime;
+  let totalStepsTaken = 0;
+
 
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
@@ -16,6 +18,29 @@ registerSketch('sk4', function (p) {
   p.draw = function () {
     p.background('#03002e');
     let now = p.millis();
+
+    let totalStepsNow = totalStepsTaken + feet.length;
+    let mileText = `Miles: ${(totalStepsNow / 1600).toFixed(2)}`;
+    let stepText = `Steps: ${totalStepsNow}`;
+    let fullText = `${stepText}   ${mileText}`;
+
+    p.textSize(20);
+    p.textFont('monospace');
+    let textWidth = p.textWidth(fullText);
+    let padding = 20;
+    let boxWidth = textWidth + padding;
+    let boxHeight = 40;
+
+    p.fill('#222');
+    p.stroke('#888');
+    p.strokeWeight(2);
+    p.rectMode(p.CENTER);
+    p.rect(p.width / 2, 40, boxWidth, boxHeight, 10);
+
+    p.fill('#fff');
+    p.noStroke();
+    p.textAlign(p.CENTER, p.CENTER);
+    p.text(fullText, p.width / 2, 40);
 
     if (now - lastStepTime >= stepInterval) {
       feet.push({

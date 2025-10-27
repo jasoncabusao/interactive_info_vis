ed0e9532b6746d992b76707afe0e730f1fc25edd
jcabusao <jcabusao@uw.edu>
2025-10-26 19:26:17 -0700

    Added base sketch setup for sketch1

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -2,13 +2,13 @@
 registerSketch('sk2', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.textFont('monospace');
   };
+
   p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    p.background('#39760E');
   };
+
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
 });

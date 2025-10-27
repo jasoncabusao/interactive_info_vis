94176cb41d1da5ee6e964726f770a93f2fa65b9a
jcabusao <jcabusao@uw.edu>
2025-10-26 19:31:59 -0700

    Added bunny frame loading and animation logic

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,5 +1,16 @@
 // Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
+  let bunnyFrames = [];
+  let currentFrame = 0;
+  let frameInterval = 100;
+  let lastFrameSwitch = 0;
+
+  p.preload = function () {
+    for (let i = 0; i < 6; i++) {
+      bunnyFrames[i] = p.loadImage(`../runningbunny/bunny${i + 1}.jpg`);
+    }
+  };
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.textFont('monospace');
@@ -7,6 +18,17 @@ registerSketch('sk2', function (p) {
 
   p.draw = function () {
     p.background('#39760E');
+
+    let now = p.millis();
+    if (now - lastFrameSwitch > frameInterval) {
+      currentFrame = (currentFrame + 1) % bunnyFrames.length;
+      lastFrameSwitch = now;
+    }
+
+    let img = bunnyFrames[currentFrame];
+    if (img) {
+      p.image(img, p.width / 2 - 150, p.height / 2 - 150, 300, 300);
+    }
   };
 
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };

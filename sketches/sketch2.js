fcbf83bbe149c715a903d15d5efc73ddd12a721b
jcabusao <jcabusao@uw.edu>
2025-10-27 09:48:23 -0700

    added mouse movement detection to simulate someone running

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -4,10 +4,11 @@ registerSketch('sk2', function (p) {
   let currentFrame = 0;
   let frameInterval = 100;
   let lastFrameSwitch = 0;
+  let lastMoveTime = 0;
 
   p.preload = function () {
     for (let i = 0; i < 6; i++) {
-      bunnyFrames[i] = p.loadImage(`runningbunny/bunny${i + 1}.jpg`);
+      bunnyFrames[i] = p.loadImage(`../runningbunny/bunny${i + 1}.jpg`);
     }
   };
 
@@ -20,6 +21,15 @@ registerSketch('sk2', function (p) {
     p.background('#39760E');
 
     let now = p.millis();
+    let isMoving = now - lastMoveTime < 100;
+
+    //movement detection
+    p.fill('#fff');
+    p.textSize(20);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.text(isMoving ? "You're running!" : "Move your mouse to run!", p.width / 2, p.height / 2);
+
+
     if (now - lastFrameSwitch > frameInterval) {
       currentFrame = (currentFrame + 1) % bunnyFrames.length;
       lastFrameSwitch = now;
@@ -29,6 +39,10 @@ registerSketch('sk2', function (p) {
     if (img) {
       p.image(img, p.width / 2 - 150, p.height / 2 - 150, 300, 300);
     }
+  };
+
+  p.mouseMoved = function () {
+    lastMoveTime = p.millis();
   };
 
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };

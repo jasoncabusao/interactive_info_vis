db1e954e35de8cea2139d6d0684ab10c1d0b0185
jcabusao <jcabusao@uw.edu>
2025-10-27 10:37:23 -0700

    added different activity modes

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,9 +1,25 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+  let walkButton, jogButton, sprintButton;
+  let mode = 'none';
+
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.textFont('monospace');
     p.angleMode(p.DEGREES);
+
+    walkButton = p.createButton('Walk');
+    jogButton = p.createButton('Jog');
+    sprintButton = p.createButton('Sprint');
+
+    walkButton.position(20, 100);
+    jogButton.position(20, 140);
+    sprintButton.position(20, 180);
+
+    walkButton.mousePressed(() => mode = 'walk');
+    jogButton.mousePressed(() => mode = 'jog');
+    sprintButton.mousePressed(() => mode = 'sprint');
   };
 
   p.draw = function () {

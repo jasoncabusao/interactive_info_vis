3fcd00c47c39df1bb62c7b2357022c526778b36a
jcabusao <jcabusao@uw.edu>
2025-11-03 00:44:58 -0800

    added axes for plot

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,4 +1,6 @@
 registerSketch('sk5', function (p) {
+  let table;
+
   p.preload = function () {
     table = p.loadTable('StateLiteracy.csv', 'csv', 'header');
   };
@@ -8,6 +10,24 @@ registerSketch('sk5', function (p) {
     p.background(255);
     p.textFont('Arial');
     p.textSize(10);
+
+    // Axes with smaller margins
+    let leftMargin = 100;
+    let rightMargin = 100;
+    let topMargin = 120;
+    let bottomMargin = 100;
+
+    p.stroke(0);
+    p.line(leftMargin, p.height - bottomMargin, p.width - rightMargin, p.height - bottomMargin); // x-axis
+    p.line(leftMargin, p.height - bottomMargin, leftMargin, topMargin); // y-axis
+
+    // Axis labels
+    p.noStroke();
+    p.textAlign(p.CENTER);
+    p.text("Poverty 150%", (p.width - leftMargin - rightMargin) / 2 + leftMargin, p.height - bottomMargin + 30);
+    p.textAlign(p.RIGHT);
+    p.text("Y-axis Metric", leftMargin - 20, (p.height - bottomMargin + topMargin) / 2);
+
   };
 
   p.draw = function () {

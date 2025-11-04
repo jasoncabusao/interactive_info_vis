015ee37bf4d22b8a7722fb84ffbf0ace604ceadf
jcabusao <jcabusao@uw.edu>
2025-11-04 10:16:07 -0800

    changed subtitle

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -20,6 +20,8 @@ registerSketch('sk5', function (p) {
 
   p.preload = function () {
     table = p.loadTable('StateLiteracy.csv', 'csv', 'header');
+    nhFlag = p.loadImage('NHFlag.png');
+    msFlag = p.loadImage('MSFlag.png');
   };
 
   p.setup = function () {
@@ -54,7 +56,7 @@ registerSketch('sk5', function (p) {
 
     // Title and subtitle
     let title = "States with Higher Poverty Tend to Have Lower Literacy — and the Gap Is Regional";
-    let subtitle = "Average adult literacy scores decline as the share of residents living below 150% of the poverty line increases. States in the Southeast and Southwest are the most affected, while New England and the Upper Midwest maintain higher literacy levels even at lower poverty rates.";
+    let subtitle = "States in the Southeast and Southwest show lower literacy than their poverty levels alone would predict, while New England maintains stronger outcomes.";
 
     // Title styling
     p.textAlign(p.CENTER, p.TOP);
@@ -120,7 +122,19 @@ registerSketch('sk5', function (p) {
     p.textAlign(p.CENTER);
     p.text("Population (%) below 150% of the poverty level", (p.width - leftMargin - rightMargin) / 2 + leftMargin, p.height - bottomMargin + 30);
     p.textAlign(p.RIGHT);
-    p.text("Average Literacy Score", leftMargin - 40, (p.height - bottomMargin + topMargin) / 2);
+    p.text("Avg. State\nLiteracy Scores", leftMargin - 40, (p.height - bottomMargin + topMargin) / 2);
+
+
+    // Light red shading
+    p.noStroke();
+    p.fill(255, 100, 100, 50);
+    p.rect(x1, yTop, x2 - x1, yBottom - yTop);
+
+    for (let pt of points) {
+      p.fill(regionColors[pt.region] || 'black');
+      p.noStroke();
+      p.ellipse(pt.x, pt.y, 12, 12);
+    }
 
 
     // New Hampshire data point
@@ -128,12 +142,14 @@ registerSketch('sk5', function (p) {
     let nhY = p.map(278.9, 240, 285, p.height - bottomMargin, topMargin) + 20;
     let nhAnnotation = "New Hampshire shows one of the highest average literacy scores in the nation, and one of the lowest poverty rates.";
     let nhTextX = nhX + 20;
-    let nhTextY = nhY - 40;
+    let nhTextY = nhY + 20;
     p.fill(0);
     p.noStroke();
     p.textAlign(p.LEFT, p.TOP);
     p.textWrap(p.WORD);
     p.text(nhAnnotation, nhTextX, nhTextY, 200);
+    // NH Flag
+    p.image(nhFlag, nhTextX+65, nhTextY - 75, nhFlag.width * .15, nhFlag.height * .15);
 
     // Mississippi data point
     let msX = p.map(0.33, 0.1, 0.35, xStart, xStart + plotWidth);
@@ -141,6 +157,8 @@ registerSketch('sk5', function (p) {
     let msAnnotation = "Mississippi has one of the highest poverty rates and among the lowest literacy scores.";
     let msTextX = msX - 220; // position to the left
     let msTextY = msY - 40;
+    //MS Flag
+    p.image(msFlag, msTextX + 350, msTextY + 35, msFlag.width*0.03, msFlag.height*0.03);
 
     p.fill(0);
     p.noStroke();
@@ -161,7 +179,7 @@ registerSketch('sk5', function (p) {
     // line from dot to annotation
     p.stroke(0);
     p.strokeWeight(1);
-    p.line(msX, msY + 93, msTextX + 300, msTextY + 110);
+    p.line(msX + 30, msY + 85, msTextX + 290, msTextY + 110);
 
 
     // Dotted outline around New Hampshire point
@@ -175,23 +193,25 @@ registerSketch('sk5', function (p) {
       let y = nhY + radius * p.sin(angle) - 60;
       p.point(x, y);
     }
-
     p.stroke(0);
     p.strokeWeight(1);
-    p.line(nhX + 90, nhY - 43, nhX + 135, nhY - 60); // from dot to annotation
+    p.line(nhX + 110, nhY - 43, nhX + 135, nhY - 60);
 
     // Annotation text
+    p.textStyle(p.BOLD);
     let annotation = "Southern states cluster together, showing consistently higher poverty and lower literacy levels compared to other regions.";
 
     // Position near the right side of the box
-    let annotationX = x2 - 160; // slightly inside from the right edge
+    let annotationX = x2 - 215; // slightly inside from the right edge
     let annotationY = yTop + 50; // slightly below the top edge
 
     p.fill(0);
     p.noStroke();
-    p.textAlign(p.LEFT, p.CENTER);
+    p.textAlign(p.CENTER, p.CENTER);
     p.textWrap(p.WORD);
-    p.text(annotation, annotationX, annotationY, 150); // 250px wide text box
+    p.text(annotation, annotationX, annotationY, 200);
+    p.textStyle(p.NORMAL); // make it bold
+
 
 
     p.textAlign(p.LEFT, p.TOP);
@@ -210,23 +230,12 @@ registerSketch('sk5', function (p) {
     let i = 0;
     for (let region in regionColors) {
       p.fill(regionColors[region]);
-      p.rect(p.width - 250, 320 + i * 20, 10, 10);
+      p.rect(p.width - 250, 250 + i * 20, 10, 10);
       p.fill(0);
       p.textAlign(p.LEFT);
-      p.text(region, p.width - 230, 325 + i * 20);
+      p.text(region, p.width - 230, 250 + i * 20);
       i++;
     }
-
-    for (let pt of points) {
-      p.fill(regionColors[pt.region] || 'black');
-      p.noStroke();
-      p.ellipse(pt.x, pt.y, 12, 12);
-    }
-
-    // Light red shading behind the dashed box
-    p.noStroke();
-    p.fill(255, 100, 100, 50);
-    p.rect(x1, yTop, x2 - x1, yBottom - yTop);
 
     // Tooltip on hover
     for (let pt of points) {
